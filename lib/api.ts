a1aa161c5063f2a95105aa4d9ab5a450f99069ee
scavvv13 <scave1013@gmail.com>
2025-03-07 19:49:38 +0800

    added env.local implemented NODE.ENV for detecting the api route when in diff dev env

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,16 @@
+///[environment detection]
+const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000" // Use localhost in dev
+    : process.env.NEXT_PUBLIC_API_URL; // Use env-provided URL in production
+
 ///[fetching announcements]
 
 export async function getAnnouncements() {
   //used in main page.tsx
   try {
-    const res = await fetch("http://localhost:3000/api/announcements", {
-      cache: "no-store", // Prevents Next.js from caching the request
+    const res = await fetch(`${API_URL}/api/announcements`, {
+      cache: "no-store",
     });
 
     if (!res.ok) throw new Error("Failed to fetch announcements");
