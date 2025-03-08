b31e202b23b5e67a2607f5cb1642d114a4dc716d
scavvv13 <scave1013@gmail.com>
2025-03-08 20:13:32 +0800

    used Link instad of <a> in not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -5,9 +7,9 @@ export default function NotFound() {
       <p className="mt-2 text-lg text-gray-600">
         The page you are looking for does not exist.
       </p>
-      <a href="/" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+      <Link href="/" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
         Go Home
-      </a>
+      </Link>
     </div>
   );
 }
