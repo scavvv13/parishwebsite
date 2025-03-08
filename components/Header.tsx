88a7d9d42f571affd0cbffc08f9fa5988722de14
scavvv13 <scave1013@gmail.com>
2025-03-08 19:35:43 +0800

    removed unused vars

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import Navbar from "./Navbar";
 import Link from "next/link";
